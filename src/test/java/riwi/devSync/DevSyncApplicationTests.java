7f540b6da2c5236ff8cfd2335fa422d56a90e862
cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2024-03-03 20:56:47 -0500

    Project

---
+++ src/test/java/riwi/devSync/DevSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package riwi.devSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
